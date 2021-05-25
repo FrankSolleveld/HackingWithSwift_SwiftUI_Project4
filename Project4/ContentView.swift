58155c0a91ebff6262b14276883413cb8b67c4b6
Frank Solleveld <fsolleveld@outlook.com>
2021-05-25 09:18:55 +0200

    Initial Commit

---
+++ Project4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project4
+//
+//  Created by Frank Solleveld on 25/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project4/Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4App.swift
+//  Project4
+//
+//  Created by Frank Solleveld on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
