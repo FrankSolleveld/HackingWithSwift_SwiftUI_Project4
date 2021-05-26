6bf5e118f2fa0c5bfcbdf0f8ed6c498fec4ced99
Frank Solleveld <fsolleveld@outlook.com>
2021-05-26 11:19:15 +0200

    main UI

--- Project4/ContentView.swift
+++ Project4/ContentView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            .labelsHidden()
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep 💤")
+                    .font(.headline)
+                Stepper(value: $sleepAmount, in: 1...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                Text("Daily coffee intake ☕️")
+                    .font(.headline)
+                Stepper(value: $coffeeAmount, in: 0...20, step: 1) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }
+            }
+            .navigationBarTitle(Text("BetterRest 😴"))
+            .navigationBarItems(trailing:
+                                    Button(action: calculateBedtime) {
+                                        Text("Calculate")
+                                    }
+            )
+        }
     }
+    
+    func calculateBedtime() {
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
