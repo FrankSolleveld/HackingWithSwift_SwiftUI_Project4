f43235a5ac93f83de730a497d396c50c95469340
Frank Solleveld <fsolleveld@outlook.com>
2021-05-26 11:38:13 +0200

    added ML model and function to precept sleep time

--- Project4/ContentView.swift
+++ Project4/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     
     @State private var wakeUp = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -43,11 +48,30 @@ struct ContentView: View {
                                         Text("Calculate")
                                     }
             )
+            .alert(isPresented: $showingAlert, content: {
+                Alert(title: Text("\(alertTitle)"), message: Text("\(alertMessage)"), dismissButton: .default(Text("OK")))
+            })
         }
     }
     
     func calculateBedtime() {
-        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        do {
+            let model: SleepCalculator = try SleepCalculator(configuration: MLModelConfiguration())
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is..."
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
     
 }
